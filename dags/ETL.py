45bc2bedc9b65c6b670c304bb494cf601d5452cf
jose3889 <jose3889@hotmail.es>
2022-11-03 13:38:17 -0300

    commit jose

--- dags/ETL.py
+++ dags/ETL.py
@@ -3,22 +3,12 @@ from airflow.decorators import task
 import snowflake.connector
 from datetime import datetime
 import pandas as pd
-import module.snow  as sn
+
 
 #conn = SnowflakeHook(snowflake_conn_id="snowflake_conn")
 
 #funcion de coneccion a snowflake
-conn = snowflake.connector.connect(
-        user=sn.snow_user,
-        password=sn.snow_password,
-        account=sn.snow_account,
-        warehouse=sn.snow_warehouse,
-        database=sn.snow_database)
 
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    cursor.execute(query)
-    cursor.close()
 
 @task #tarea de extraccion de datos del WB y WHO
 def extract_data() -> pd.DataFrame:
@@ -33,6 +23,20 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
 @task #tarea de guardar archivo csv comprimido a stage de snowflake
 def load_data(df: pd.DataFrame):
     import tempfile
+    import module.snow  as sn
+    
+    conn = snowflake.connector.connect(
+        user=sn.snow_user,
+        password=sn.snow_password,
+        account=sn.snow_account,
+        warehouse=sn.snow_warehouse,
+        database=sn.snow_database)
+
+    def execute_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+
     temp_dir=tempfile.mkdtemp()
     df.to_csv(temp_dir +'/EV_limpio.csv', index=False)
     sql = f"PUT file://{temp_dir}/EV_limpio.csv @DATA_STAGE auto_compress=true"
