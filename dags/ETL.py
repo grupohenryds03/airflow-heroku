53de5111ec58f394ba6ebb279653be586d368174
jose3889 <jose3889@hotmail.es>
2022-11-03 20:06:50 -0300

    commit jose

--- dags/ETL.py
+++ dags/ETL.py
@@ -1,28 +1,22 @@
 from airflow.models import DAG
-from airflow.models import Variable
 from airflow.decorators import task
-from datetime import datetime
 import snowflake.connector
+from datetime import datetime
 import pandas as pd
 
 
-conn = snowflake.connector.connect(
-    user='grupods03',
-    password='Henry2022#',
-    account='nr28668.sa-east-1.aws',
-    warehouse='DW_EV',
-    database="LAKE"
-    )
 #funcion de coneccion a snowflake
+conn = snowflake.connector.connect(
+    snow_user='grupods03',
+    snow_password='Henry2022#',
+    snow_account='nr28668.sa-east-1.aws',
+    snow_warehouse='DW_EV',
+    snow_database="LAKE")
+
 def execute_query(connection, query):
     cursor = connection.cursor()
     cursor.execute(query)
     cursor.close()
-
-#conn = SnowflakeHook(snowflake_conn_id="snowflake_conn")
-
-
-
 
 @task #tarea de extraccion de datos del WB y WHO
 def extract_data() -> pd.DataFrame:
@@ -32,26 +26,21 @@ def extract_data() -> pd.DataFrame:
 @task #tarea de limpieza de datos
 def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     import module.transform as tran
-    
     return tran.etl_transform(df) #llama a la funcion de limpieza (rodri)
 
 @task #tarea de guardar archivo csv comprimido a stage de snowflake
 def load_data(df: pd.DataFrame):
     import tempfile
-    
     temp_dir=tempfile.mkdtemp()
-    
     df.to_csv(temp_dir +'/EV_limpio.csv', index=False)
     sql = f"PUT file://{temp_dir}/EV_limpio.csv @DATA_STAGE auto_compress=true"
     execute_query(conn, sql)
-    #dwh_hook.get_first(sql)
 
 #creación de las tareas con cronograma anual
 with DAG(
    "ETL",
    start_date=datetime(year=2022, month=10, day=22),
-   schedule_interval='@yearly',
-   catchup=False
+   schedule_interval='@yearly'
    ) as dag:
         df_crudo = extract_data() #sale data cruda
         df_limpio=transform_data(df_crudo) #entra data cruda, sale limpia
